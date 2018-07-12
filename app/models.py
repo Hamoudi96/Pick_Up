1f161732cc7324ccfa60604e4611e6fe470ec54f
Oumar <oumar@oumars-mbp.raleigh.ibm.com>
2018-07-12 18:36:56 -0400

    push

--- app/models.py
+++ app/models.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
+
+class Users(models.Model):
+    user = models.OneToOneField(User, default=0)
+    
+class FildsLocation(models.Model):
+    field_name = models.CharField(max_length=200, default='')
