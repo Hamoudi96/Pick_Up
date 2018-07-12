8e52887bd1b144c0d289a97af82dd499676fd6f8
Brandon Berry <Brandon.Jinarain.Berry@ibm.com>
2018-07-12 18:45:24 -0400

    end of day

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.http import JsonResponse
 from django.http import Http404
 from django.shortcuts import render
+import googlemaps
 
 
 def index(request):
@@ -19,3 +20,13 @@ def handler404(request):
 
 def handler500(request):
     return render(request, '500.html', status=500)
+
+def location(request):
+    gmaps = googlemaps.Client(key='AIzaSyCKPH1UUPYRIaQ71Ep-8urwmBNKlSIBdHw')
+    location1 = '1600 Amphitheatre Parkway, Mountain View, CA'
+    location2 = '3 Prarie View Ct, Durham, NC'
+    return JsonResponse(pullDistanceFrom(gmaps.distance_matrix(location1,location2)), safe=False)
+
+def pullDistanceFrom(gmapsResult):
+    return {"meterDist" : gmapsResult["rows"][0]["elements"][0]["distance"]["value"]}
+
